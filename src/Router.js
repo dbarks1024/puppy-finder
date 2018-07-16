2afcb9fdec5984947a8937f63008bf5397d367e7
dbarks1024 <dbarker@bignerdranch.com>
2018-07-16 17:11:44 -0400

    Add tabs to router

--- src/Router.js
+++ src/Router.js
@@ -1,32 +1,49 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { Scene, Router, Actions } from 'react-native-router-flux';
 import SwipeDogSelect from './components/SwipeDogSelect';
 import Settings from './components/Settings';
 import BreedList from './components/settings/BreedList';
 
+const TabIcon = ({ selected, title }) => {
+    return (
+      <Text style={{ color: selected ? 'red' : 'black' }}>{title}</Text>
+    );
+  };
+
 
 const RouterComponent = () => {
     return (
         <Router>
-            <Scene key='main' >
+            <Scene key='root' hideNavBar >
                 <Scene 
-                key='swipeDogSelect' 
-                component={SwipeDogSelect} 
-                title='Pick Your Pup' 
-                leftTitle='Settings'
-                onLeft={() => Actions.settings()}
-                /> 
-                <Scene
-                key='settings'
-                component={Settings}
-                title='Settings'
-                initial
-                />
-                <Scene 
-                key='breedList'
-                component={BreedList}
-                title='Breeds'
-                />
+                key='tabbar'
+                tabs
+                tabBarStyle={{ backgroundColor: '#FFFFFF' }} 
+                >
+                    <Scene key="search" title="Search" icon={TabIcon}>
+                        <Scene 
+                        key='swipeDogSelect' 
+                        component={SwipeDogSelect} 
+                        title='Pick Your Pup' 
+                        leftTitle='Settings'
+                        onLeft={() => Actions.settings()}
+                        initial
+                        /> 
+                    </Scene>
+                    <Scene key="settings" title="Settings" icon={TabIcon}>
+                        <Scene
+                        key='settings'
+                        component={Settings}
+                        title='Settings'
+                        />
+                        <Scene 
+                        key='breedList'
+                        component={BreedList}
+                        title='Breeds'
+                        />   
+                    </Scene>     
+                </Scene> 
             </Scene>
         </Router>
     );
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
-import { Container, Text, Header, Content, Card, CardItem, Icon, Right } from 'native-base';
+import { Container, Text, Content, Card, CardItem, Icon, Right } from 'native-base';
 
 class Settings extends Component {
     render() {
         return (
             <Container>
-                <Header />
                 <Content>
                     <Card>
                         <CardItem button onPress={() => Actions.breedList()}>
