664a00ad7b10ca99b1f9af352ca472b774b1639d
dbarks1024 <dbarker@bignerdranch.com>
2018-07-19 09:28:26 -0400

    Add Tabs details

--- src/components/ShowDog.js
+++ src/components/ShowDog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
-import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Left, Right, Body } from 'native-base';
+import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Left, Right, Body, Tabs, Tab, List } from 'native-base';
 import { Actions } from 'react-native-router-flux'; 
 import { connect } from 'react-redux';
 import { removeDog, addDog, findDogById } from '../actions';
@@ -40,12 +40,56 @@ class ShowDog extends Component {
                     </CardItem>
                     <CardItem>
                         <Body>
-                        <Image source={{ uri: dog.photos[3].$t }} style={{ height: 350, width: 350, flex: 1 }} />
-                        <Text>
-                            {dog.description}
-                        </Text>
+                            <Image source={{ uri: dog.photos[3].$t }} style={{ height: 350, width: 350, flex: 1 }} />
                         </Body>
                     </CardItem>
+                    <CardItem>
+                        <Tabs>
+                            <Tab heading="Description">
+                                <CardItem>
+                                    <Body>
+                                        <Text>
+                                            {dog.description}
+                                        </Text>
+                                    </Body>
+                                </CardItem>
+                            </Tab>
+                            <Tab heading="Details">
+                                <CardItem>
+                                    <Body>
+                                        <Text>
+                                            Sex: {dog.sex}
+                                        </Text>
+                                        <Text>
+                                            Age: {dog.age}
+                                        </Text>
+                                        <Text>
+                                        About: 
+                                        </Text>
+                                        <List
+                                        dataArray={dog.options}
+                                        renderRow={(item) =>                                          
+                                            <Text>
+                                                {item.$t}
+                                            </Text>
+                                        }
+                                        />                                           
+                                    </Body>
+                                </CardItem>
+                            </Tab>
+                            <Tab heading="Contact">
+                                <CardItem>
+                                    <Body>
+                                        <Text>{dog.contact.phone.$t && dog.contact.phone.$t}</Text>
+                                        <Text>{dog.contact.email.$t && dog.contact.email.$t}</Text>
+                                        <Text>{dog.contact.city.$t && dog.contact.city.$t}</Text>
+                                        <Text>{dog.contact.state.$t && dog.contact.state.$t}</Text>
+                                    </Body>
+                                </CardItem>
+                            </Tab>
+                        </Tabs>
+                    </CardItem>
+                    
                     <CardItem>
                         <Left>
                         <Button
