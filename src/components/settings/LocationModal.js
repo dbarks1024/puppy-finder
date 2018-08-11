92d40ff26ed9b0a7afab4f7f77b3b7c43206b7fa
dbarks1024 <dbarker@bignerdranch.com>
2018-08-11 15:24:04 -0500

    Style modal

--- src/components/settings/LocationModal.js
+++ src/components/settings/LocationModal.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import Modal from "react-native-modal";
 import { connect } from 'react-redux';
-import { View, Text } from 'native-base';
+import { View, Text, Container } from 'native-base';
 import Location from './Location';
 
 class LocationModal extends Component {
   render() {
       return ( 
-        <View>
+        <View
+        style={styles.containerStyle}
+        >
           <Modal
           animationIn='slideInLeft'
           transparent
           isVisible
           onRequestClose={() => {}}
-          style={styles.containerStyle}
           >
             <View style={styles.cardStyle} >
-              <Text>Please input a valid zip code</Text>
-              <Location />
+              <Container>
+                <Text>Please input a valid zip code</Text>
+                <Location />
+              </Container>
             </View>
           </Modal>
         </View>   
@@ -27,16 +30,14 @@ class LocationModal extends Component {
 
 const styles = {
   containerStyle: {
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-    },
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
   },
   cardStyle: {
     backgroundColor: "white",
-    padding: 22,
-    justifyContent: "center",
+    border: 20,
     alignItems: "center",
     borderRadius: 4,
     borderColor: "rgba(0, 0, 0, 0.1)",
