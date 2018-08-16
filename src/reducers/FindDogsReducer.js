4dec4f46d4141691f247df7c3f5204fad624b8fa
dbarks1024 <dbarker@bignerdranch.com>
2018-08-15 22:34:01 -0400

    move to 2 spaces

--- src/reducers/FindDogsReducer.js
+++ src/reducers/FindDogsReducer.js
@@ -1,30 +1,29 @@
 import { FOUND_DOGS, FINDING_DOGS, SHOW_DOG, LAST_OFFSET, ERROR } from '../actions/types';
 
 const INITIAL_STATE = {
-    dogs: [],
-    findingDogs: true,
-    lastOffset: 0,
-    error: '',
+  dogs: [],
+  findingDogs: true,
+  lastOffset: 0,
+  error: '',
 };
 
 export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case FOUND_DOGS:
-            return { ...state,
-                dogs: action.payload
-            };
-        case FINDING_DOGS: 
-            return { ...state,
-                findingDogs: action.payload
-            };
-        case SHOW_DOG:
-        console.log(action.payload);
-            return state;
-        case LAST_OFFSET:
-            return { ...state, lastOffset: action.payload };
-        case ERROR:
-            return { ...state, error: action.payload };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case FOUND_DOGS:
+      return { ...state,
+          dogs: action.payload
+      };
+    case FINDING_DOGS: 
+      return { ...state,
+          findingDogs: action.payload
+      };
+    case SHOW_DOG:
+      return state;
+    case LAST_OFFSET:
+      return { ...state, lastOffset: action.payload };
+    case ERROR:
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
 };
